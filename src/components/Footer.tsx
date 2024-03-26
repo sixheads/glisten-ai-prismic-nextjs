22aa570b6e43e72d54e6e23836234aadfb5b4352
John Fry <john@sixheads.com>
2024-03-26 14:48:08 +1100

    Add WordMark, Header and Footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import WordMark from "./WordMark";
+
+export default function Footer() {
+  return (
+    <div>
+      <WordMark />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import WordMark from "./WordMark";
+
+export default function Header() {
+  return (
+    <div>
+      <WordMark />
+    </div>
+  );
+}
