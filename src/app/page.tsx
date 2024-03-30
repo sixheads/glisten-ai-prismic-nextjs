cdbd0a338ee4167bc2e2a3fa9624f31def25a800
John Fry <john@sixheads.com>
2024-03-30 12:38:04 +1100

    Add fields to Hero slice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Metadata } from 'next';
+import { Metadata } from "next";
 
-import { SliceZone } from '@prismicio/react';
-import * as prismic from '@prismicio/client';
+import { SliceZone } from "@prismicio/react";
+import * as prismic from "@prismicio/client";
 
-import { createClient } from '@/prismicio';
-import { components } from '@/slices';
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
 // This component renders your homepage.
 //
@@ -14,14 +14,14 @@ import { components } from '@/slices';
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getByUID('page', 'home');
+  const home = await client.getByUID("page", "home");
 
   return {
     title: prismic.asText(home.data.title),
     description: home.data.meta_description,
     openGraph: {
       title: home.data.meta_title ?? undefined,
-      images: [{ url: home.data.meta_image.url ?? '' }],
+      images: [{ url: home.data.meta_image.url ?? "" }],
     },
   };
 }
@@ -29,9 +29,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Index() {
   // The client queries content from the Prismic API
   const client = createClient();
-  const home = await client.getByUID('page', 'home');
+  const home = await client.getByUID("page", "home");
 
-  return (
-    <SliceZone slices={home.data.slices} components={components} />
-  );
+  return <SliceZone slices={home.data.slices} components={components} />;
 }
